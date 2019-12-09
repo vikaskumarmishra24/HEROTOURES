e256dff04646341f9f43f212d58132bf9b5d2de1
Vikas Kumar Mishra <vikas.mishra@espire.com>
2019-12-09 11:00:51 +0530

    First Commit

---
+++ src/app/hero.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import{Hero} from './hero';
+
+import {HEROES} from './mock-heroes'
+
+import { from ,Observable, of} from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private messageService:MessageService) { }
+
+  getHeroes(): Observable<Hero[]> {
+
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+}
